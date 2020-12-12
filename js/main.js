1cec191695d78aa2abc4bd6ea03797b0051bb780
iDestar <destarpwnz@gmai.com>
2020-12-12 04:16:24 +0300

    sum

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-"use strict"
-let button = document.querySelector("#cart-button");
-let modal = document.querySelector(".modal");
-let close = document.querySelector(".close");
+'use strict';
+let button = document.querySelector('#cart-button');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
 
-function taggleModal () {
-    modal.classList.toggle('is-open');
+function taggleModal() {
+  modal.classList.toggle('is-open');
 }
 
 button.addEventListener(`click`, taggleModal);
 close.addEventListener(`click`, taggleModal);
 
-foo
+foo;
